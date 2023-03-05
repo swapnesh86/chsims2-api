704c547d5cc4cd69f4e16d9a3ccc39e1e4a5bab9
swapnesh86 <swapnesh.j@gmail.com>
2023-03-05 11:25:51 +0800

    Accidentally removed authentication for membership

--- routes/membershipRoutes.js
+++ routes/membershipRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const membershipController = require('../controllers/membershipController')
 const verifyJWT = require('../middleware/verifyJWT')
 
-//router.use(verifyJWT)
+router.use(verifyJWT)
 
 router.route('/')
     .get(membershipController.getAllMembership)
