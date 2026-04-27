63d420219f7e4ef6602924fbac1b28ce92802a1a
swapnesh86 <swapnesh.j@gmail.com>
2026-04-27 22:29:46 +0800

    Add allowed methods and headers for CORS

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -8,6 +8,8 @@ const corsOptions = {
             callback(new Error('Not allowed by CORS'))
         }
     },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Explicitly allow OPTIONS
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
     optionsSuccessStatus: 200
 }
